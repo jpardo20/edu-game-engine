3e6ece6a661d1a409bd7e74e4b69e7bd665f5d81
jpardo20 <jpardo20@xtec.cat>
2026-05-15 16:26:07 +0200

    refactor: enable ES modules and initialize Game class

---
+++ js/core/Game.js
@@ -0,0 +1,15 @@
+export default class Game {
+
+    constructor() {
+
+        console.log("Game engine initialized");
+
+    }
+
+    start() {
+
+        console.log("Game started");
+
+    }
+
+}
