4e2e7e6d88de8eca24cd414ac39edffaccf21ab2
jpardo20 <jpardo20@xtec.cat>
2026-05-18 16:51:05 +0200

    feat: improve scoreboard layout and team display

--- js/render/renderScoreboard.js
+++ js/render/renderScoreboard.js
@@ -3,7 +3,7 @@ export function pintaMarcador(
     equips,
     equipActiu,
     config
-)   {
+) {
 
     if (!elements.marcador) return;
 
@@ -13,106 +13,66 @@ export function pintaMarcador(
 
         return `
     <div class="score-row">
-
-  <div class="team-members-side">
-    ${equip.membresEq
-                .map(m => `${m.firstname} ${m.lastname}`)
-                .join("<hr>")}
-  </div>
-
-  <div class='score ${isActive ? "active" : ""}'>
-
-      <div class="score-header">
-        <span class="team-name" style="background:${equip.color}">
-          ${equip.nomEq}
-        </span>
-
-      </div>
-
-      <!-- VIRALITAT -->
-
-      <div class="metric">
-
+        <div class='score ${isActive ? "active" : ""}'>
+        <div class="score-main">
+        <div class="score-header">
+            <span class="team-name" style="background:${equip.color}">${equip.nomEq}</span>
+        
+        </div>
+        
+        <!-- Primera barra -->
+        <div class="metric">
         <div class="metric-info">
-            <span 
-                class="metric-label viralitat tooltip"
-                data-tooltip="${config?.scoreSystem?.tooltip?.[0]}">
-                ${
-    config?.scoreSystem?.labels?.[0]
-    || "Barra 1"
-}
+        <span class="metric-label primeraBarra tooltip"
+        data-tooltip="${config?.scoreSystem?.barr_info?.[0]}">
+        ${config?.scoreSystem?.labels?.[0]
+            || "Barra 1"}
             </span>
-
-          <span class="value ${equip.barra1 < 0 ? "neg" : "pos"}">
-            ${equip.barra1}
-          </span>
+                        <span class="value ${equip.barra1 < 0 ? "neg" : "pos"}">
+                        ${equip.barra1}
+                        </span>
+                        </div>
+                        <div class="bar">
+                        <div class="fill pop pos" style="
+                        width:${Math.max(0, equip.barra1) * 10}%">
+                        </div>
+                        <div class="fill pop neg" style="
+                        width:${Math.max(0, -equip.barra1) * 10}%">
+                        </div>
+                        </div>
+                        </div>${config?.scoreSystem?.bars === 2? `
+                <!-- Segona barra -->
+                <div class="metric">
+                <div class="metric-info">
+                <span class="metric-label segonaBarra tooltip"
+                data-tooltip="${config?.scoreSystem?.barr_info?.[1]}">
+                ${config?.scoreSystem?.labels?.[1]
+                    || "Barra 1"}
+                    </span>
+                    <span class="value ${equip.barra2 < 0 ? "neg" : "pos"}">
+                    ${equip.barra2}
+                    </span>
+                    </div>
+                    <div class="bar">
+                    <div class="fill segonaBarra pos" style="
+                    width:${Math.max(0, equip.barra2) * 10}%">
+                    </div>
+                    <div class="fill segonaBarra neg" style="
+                    width:${Math.max(0, -equip.barra2) * 10}%">
+                    </div>
+                    </div>
+                    </div>
+                    `: ""   }
+                    
+                    </div>
+                    <div class="team-members-side">
+                    ${equip.membresEq
+                        .map(m => ` - ${m.firstname} ${m.lastname}`)
+                        .join("<br>")}
+                        <p class="casella" style="font-size:0.9rem;">Casella ${equip.posicioTaulell}</p>
+                            </div>
+            </div>
         </div>
-
-        <div class="bar">
-          <div
-            class="fill pop pos"
-            style="
-                width:${Math.max(0, equip.barra1) * 10}%">
-          </div>
-
-          <div
-            class="fill pop neg"
-            style="
-                width:${Math.max(0, -equip.barra1) * 10}%">
-          </div>
-        </div>
-
-      </div>
-
-      ${
-        config?.scoreSystem?.bars === 2
-            ? `
-
-      <!-- PENSAMENT CRÍTIC -->
-
-      <div class="metric">
-
-        <div class="metric-info">
-            <span 
-                class="metric-label crit tooltip"
-                data-tooltip="${config?.scoreSystem?.tooltip?.[1]}">
-                ${
-    config?.scoreSystem?.labels?.[1]
-    || "Barra 1"
-}
-            </span>
-
-          <span class="value ${equip.barra2 < 0 ? "neg" : "pos"}">
-            ${equip.barra2}
-          </span>
-        </div>
-
-        <div class="bar">
-          <div
-            class="fill crit pos"
-            style="
-                width:${Math.max(0, equip.barra2) * 10}%">
-          </div>
-
-          <div
-            class="fill crit neg"
-                style="width:${Math.max(0, -equip.barra2) * 10}%">
-          </div>
-        </div>
-
-      </div>
-
-      `
-            : ""
-    }
-
-        <span class="pos" style="float:right; font-size:0.9rem;">
-          Casella ${equip.posicioTaulell}
-        </span>
-
-  </div>
-
-</div>
-`;
+  `;
     }).join("");
 }
